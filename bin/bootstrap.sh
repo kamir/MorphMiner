fa929b23ffec7e27a800532168a6bde63fbc081d
Mirko Kämpf <mirko.kaempf@cloudera.de>
2014-11-16 15:00:30 +0100

    minor fix

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,10 +1,9 @@
-set version=0.8.1
 cd ..
 mvn clean
 mvn install:install-file -Durl=file:///home/cloudera/MorphMiner/lib -Dfile=lib/sshxcute-1.0.jar -DgroupId=net.neoremind.sshxcute -DartifactId=sshxcute -Dpackaging=jar -Dversion=1.0
 mvn install:install-file -Durl=file:///home/cloudera/MorphMiner/lib -Dfile=lib/json-lib.jar -DgroupId=net.sf.json-lib -DartifactId=json-lib -Dpackaging=jar -Dversion=2.4
 mvn compile package
-cp target/morphminer-$version-SNAPSHOT-jar-with-dependencies.jar bin/mm-latest.jar
+cp target/morphminer-0.8.2-SNAPSHOT-jar-with-dependencies.jar bin/mm-latest.jar
 java -jar ./bin/mm-latest.jar;./lib/json-lib.jar;./lib/sshxcute-1.0.jar de.bitocean.mm.MorphMinerTool 
 cd bin
 
