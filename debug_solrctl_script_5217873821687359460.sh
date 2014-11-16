23e79a824553030a7c62d0cb0434aca8462d09b8
Mirko Kämpf <mirko.kaempf@cloudera.de>
2014-11-16 12:39:47 +0100

    prep for testdrive ...

--- debug_solrctl_script_5217873821687359460.sh
+++
@@ -1,2 +0,0 @@
-solrctl --solr 127.0.0.1:8983/solr --zk 127.0.0.1:2181/solr instancedir --update FAQMails02Collection /home/training/FAQMails02SearchConfig
-solrctl --solr 127.0.0.1:8983/solr --zk 127.0.0.1:2181/solr collection --reload FAQMails02Collection
