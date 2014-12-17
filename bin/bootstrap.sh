1d0764873e23e5d08d1f1c8e9288499a66169205
Mirko Kämpf <mirko.kaempf@cloudera.de>
2014-12-17 17:16:04 +0000

    updated the bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,6 +3,7 @@ mvn clean
 mvn install:install-file -Durl=file:///home/cloudera/MorphMiner/lib -Dfile=lib/sshxcute-1.0.jar -DgroupId=net.neoremind.sshxcute -DartifactId=sshxcute -Dpackaging=jar -Dversion=1.0
 mvn install:install-file -Durl=file:///home/cloudera/MorphMiner/lib -Dfile=lib/json-lib.jar -DgroupId=net.sf.json-lib -DartifactId=json-lib -Dpackaging=jar -Dversion=2.4
 mvn compile package assembly:single
+rm bin/mm-latest.jar
 cp target/morphminer-0.9.1-SNAPSHOT-jar-with-dependencies.jar bin/mm-latest.jar
-java -jar ./bin/mm-latest.jar;./lib/json-lib.jar;./lib/sshxcute-1.0.jar de.bitocean.mm.MorphMinerTool 
+java -jar ./target/morphminer-0.9.1-SNAPSHOT-jar-with-dependencies.jar;./bin/mm-latest.jar;./lib/json-lib.jar;./lib/sshxcute-1.0.jar de.bitocean.mm.MMAppRunner 
 cd bin
